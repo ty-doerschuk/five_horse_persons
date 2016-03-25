5dcf730efe60a99945ef99a8d29b313e03037516
Ian Wudarski <email": null>
2016-03-25 15:59:33 -0500

    set up user page

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -17,12 +17,21 @@ end
 
 post '/users/login' do
   @user = User.find_by(username: params[:username])
-
+  p params
   if @user.authenticate(params[:password])
       session[:id] = @user.id
       redirect "/users/#{@user.id}"
   else
     @errors = ["Invalid Username/Password"]
     erb :index
+  end
+end
+
+get '/users/:id' do
+  if session[:id] == nil
+    redirect '/'
+  else
+    @user = User.find(session[:id])
+    erb :'users/user_page'
   end
 end
