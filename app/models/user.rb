571c4fd90c8af30d0a99e2619fab649d89e67df7
Ian Wudarski <email": null>
2016-03-25 15:02:20 -0500

    Add login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   has_many  :rounds
   has_many  :guesses, through: :rounds
+
+  validates :username, uniqueness: true
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
+
 end
